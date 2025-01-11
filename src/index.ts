8e34e5a9198625dadeeda45ea18fd563eedbd54e
ip <ivan.poluianov@gmail.com>
2025-01-11 14:31:42 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ async function process() {
     console.log("STARTED");
     const keypair = new Ed25519Keypair();
     const client = new SuiClient({
-        url: getFullnodeUrl('mainnet'), // или 'mainnet', в зависимости от вашей среды
+        url: getFullnodeUrl('mainnet'),
     });
 
     const packageObjectId = '0xbe66e3956632c8b8cb90211ecb329b9bb03afef9ba5d72472a7c240d3afe19fd'; // замените на ваш PACKAGE_ID
@@ -18,10 +18,10 @@ async function process() {
     tx.setSender("0x24789498deeb4b84c73e58554a73912a2c6a2358905903ac68f9a72818c64766");
     // 0x261fb14f034bf488b8bfdeb263f081b5073883269368e258852f34deeae205d2 - fund
     tx.moveCall({
-        target: `${packageObjectId}::fund::ex2`, // замените на ваш модуль и функцию
+        target: `${packageObjectId}::fund::ex2`,
         arguments: [
             tx.object('0x261fb14f034bf488b8bfdeb263f081b5073883269368e258852f34deeae205d2'), // замените на ваш аргумент
-        ], // замените на ваши аргументы
+        ],
     });
 
     let txBytes = await tx.build({
