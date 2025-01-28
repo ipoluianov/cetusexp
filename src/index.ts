720f9f1a61e1edd0669fb72ff7abe40de253077b
ip <ivan.poluianov@gmail.com>
2025-01-28 12:47:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,81 @@ import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
 import { Transaction } from '@mysten/sui/transactions';
 
+import CetusClmmSDK, { adjustForSlippage, d, initCetusSDK, Percentage, printTransaction, RewarderAmountOwed } from '@cetusprotocol/cetus-sui-clmm-sdk'
+import { DEV_ADDR, POOL_DEEP_SUI } from './config';
+import { swap } from './swap';
+
 async function tohex(bs : Uint8Array) {
     return Array.prototype.map.call(bs, x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
+const POOL_ID = "0xe01243f37f712ef87e556afb9b1d03d0fae13f96d324ec912daffc339dfdcbd2"
+const POS_ID = "0x2bbc512dbd34d27d17f69ef3897a43583784dedd46d37f47d755fb6ea8615c2b"
+//const POS_ID = "0x8d117d05348291a0d0c7e42bf9ccaf0dfcaa40c2a3f31b477d92734a9653e633"
+
+
+function toDollars(coinName : string, amount : number) : number {
+    if (coinName.indexOf("::SUI") != -1) {
+        let sui = amount / 1000000000 * 4.00;
+        return sui;
+    }
+
+    if (coinName.indexOf("::DEEP") != -1) {
+        let deep = amount / 1000000 * 0.26;
+        return deep;
+    }
+
+    return -1;
+}
+
 async function process() {
-    console.log("STARTED");
-    const keypair = new Ed25519Keypair();
-    const client = new SuiClient({
-        url: getFullnodeUrl('mainnet'),
-    });
+    //console.log("STARTED");
+    
+    const network = "mainnet";
+    const fullNodeUrl = "https://fullnode.mainnet.sui.io:443"
+    const simulationAccount = DEV_ADDR
+    const sdk = initCetusSDK({ network, fullNodeUrl, simulationAccount })
+    sdk.senderAddress = DEV_ADDR;
 
-    const packageObjectId = '0xbe66e3956632c8b8cb90211ecb329b9bb03afef9ba5d72472a7c240d3afe19fd'; // замените на ваш PACKAGE_ID
-    const tx = new Transaction();
-    tx.setSender("0x24789498deeb4b84c73e58554a73912a2c6a2358905903ac68f9a72818c64766");
-    // 0x261fb14f034bf488b8bfdeb263f081b5073883269368e258852f34deeae205d2 - fund
-    tx.moveCall({
-        target: `${packageObjectId}::fund::ex2`,
-        arguments: [
-            tx.object('0x261fb14f034bf488b8bfdeb263f081b5073883269368e258852f34deeae205d2'), // замените на ваш аргумент
-        ],
-    });
+    const pool = await sdk.Pool.getPool(POOL_ID)
 
-    let txBytes = await tx.build({
-        client,
-    });
+    //console.log("pool", JSON.stringify(pool, null, 2))
+    //console.log("coinTypeA", pool.coinTypeA)
+    //console.log("coinTypeB", pool.coinTypeB)
 
-    let txBytesHex = tohex(txBytes);
+    for (let i = 0; i < 1000; i++) {
+        const posRewardersAmount = await sdk.Rewarder.batchFetchPositionRewarders([POS_ID])
+        const fees = await sdk.Position.batchFetchPositionFees([POS_ID])
 
-    console.log("STOPPED", txBytesHex);
+        //console.log("posRewardersAmount", JSON.stringify(posRewardersAmount, null, 2))
+        //console.log("fees", JSON.stringify(fees, null, 2))
+
+        let item1 = posRewardersAmount[POS_ID][0];
+        let item2 = posRewardersAmount[POS_ID][1];
+        let itemFeeA = fees[POS_ID].feeOwedA;
+        let itemFeeB = fees[POS_ID].feeOwedB;
+
+        let rewards1 = toDollars(item1.coin_address, item1.amount_owed.toNumber());
+        let rewards2 = toDollars(item2.coin_address, item2.amount_owed.toNumber());
+        let feesA = toDollars(pool.coinTypeA, itemFeeA.toNumber());
+        let feesB = toDollars(pool.coinTypeB, itemFeeB.toNumber());
+
+        /*console.log("Item", item1.coin_address, item1.amount_owed.toNumber(), rewards1);
+        console.log("Item", item2.coin_address, item2.amount_owed.toNumber(), rewards2);
+        console.log("Item", pool.coinTypeA, itemFeeA.toNumber(), feesA);
+        console.log("Item", pool.coinTypeB, itemFeeB.toNumber(), feesB);*/
+
+        let totalYield = rewards1 + rewards2 + feesA + feesB;
+        console.log(totalYield.toFixed(4));
+
+        // write value to file
+        let fs = require('fs');
+        fs.writeFileSync('totalYield.txt', totalYield.toFixed(4));
+
+        await new Promise(r => setTimeout(r, 10000));
+    }
+
+    //console.log("STOPPED1");
 }
 
 process();
