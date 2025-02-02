1fe3a911242807d857418024ab31204a6310d66c
ip <ivan.poluianov@gmail.com>
2025-02-02 14:00:53 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ async function tohex(bs : Uint8Array) {
 }
 
 const POOL_ID = "0xe01243f37f712ef87e556afb9b1d03d0fae13f96d324ec912daffc339dfdcbd2"
-const POS_ID = "0xd7a4209fad58feae95da845d229fd74393454719e54f6ea5ea9ddce9f5985f2a"
+const POS_ID = "0xa8847ecc7dd5652256a542b8b68f2d76e6ccde1a7dbda63f970fc3289fe8af27"
 //const POS_ID = "0x8d117d05348291a0d0c7e42bf9ccaf0dfcaa40c2a3f31b477d92734a9653e633"
 
 
