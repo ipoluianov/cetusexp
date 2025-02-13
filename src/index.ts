d7c7c663fff5d0c38b8c3f65fae398167803545a
ip <ivan.poluianov@gmail.com>
2025-02-13 19:40:01 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import CetusClmmSDK, { adjustForSlippage, d, initCetusSDK, Percentage, printTran
 import { DEV_ADDR, POOL_DEEP_SUI } from './config';
 import { swap } from './swap';
 
-async function tohex(bs : Uint8Array) {
+async function tohex(bs: Uint8Array) {
     return Array.prototype.map.call(bs, x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
@@ -17,7 +17,7 @@ const POS_ID = "0xdd3b8a9759e049eed8d0547fc005547c9a7f48f0270741e32c4e36ea0b0433
 //const POS_ID = "0x8d117d05348291a0d0c7e42bf9ccaf0dfcaa40c2a3f31b477d92734a9653e633"
 
 
-function toDollars(coinName : string, amount : number) : number {
+function toDollars(coinName: string, amount: number): number {
     if (coinName.indexOf("::SUI") != -1) {
         let sui = amount / 1000000000 * 3.5;
         return sui;
@@ -32,8 +32,6 @@ function toDollars(coinName : string, amount : number) : number {
 }
 
 async function process() {
-    //console.log("STARTED");
-    
     const network = "mainnet";
     const fullNodeUrl = "https://fullnode.mainnet.sui.io:443"
     const simulationAccount = DEV_ADDR
@@ -42,40 +40,64 @@ async function process() {
 
     const pool = await sdk.Pool.getPool(POOL_ID)
 
-    //console.log("pool", JSON.stringify(pool, null, 2))
-    //console.log("coinTypeA", pool.coinTypeA)
-    //console.log("coinTypeB", pool.coinTypeB)
+    while (true) {
+        console.log('get positions of one pool by owner address');
+        const res = await sdk.Position.getPositionList('0x4bb32c583f8c7f81df987115d6140737a8ea0900c900c0f110d7d36485fee2a9', [
+            POOL_ID,
+        ], false)
+        if (res.length == 0) {
+            console.log('no position found');
+            continue;
+        }
 
-    for (let i = 0; i < 1000000; i++) {
-        const posRewardersAmount = await sdk.Rewarder.batchFetchPositionRewarders([POS_ID])
-        const fees = await sdk.Position.batchFetchPositionFees([POS_ID])
+        let posObjectId = res[0].pos_object_id;
 
-        //console.log("posRewardersAmount", JSON.stringify(posRewardersAmount, null, 2))
-        //console.log("fees", JSON.stringify(fees, null, 2))
+        console.log('get positions of one pool by owner address', posObjectId);
 
-        let item1 = posRewardersAmount[POS_ID][0];
-        let item2 = posRewardersAmount[POS_ID][1];
-        let itemFeeA = fees[POS_ID].feeOwedA;
-        let itemFeeB = fees[POS_ID].feeOwedB;
+        //return;
 
-        let rewards1 = toDollars(item1.coin_address, item1.amount_owed.toNumber());
-        let rewards2 = toDollars(item2.coin_address, item2.amount_owed.toNumber());
-        let feesA = toDollars(pool.coinTypeA, itemFeeA.toNumber());
-        let feesB = toDollars(pool.coinTypeB, itemFeeB.toNumber());
+        //console.log("pool", JSON.stringify(pool, null, 2))
+        //console.log("coinTypeA", pool.coinTypeA)
+        //console.log("coinTypeB", pool.coinTypeB)
 
-        /*console.log("Item", item1.coin_address, item1.amount_owed.toNumber(), rewards1);
-        console.log("Item", item2.coin_address, item2.amount_owed.toNumber(), rewards2);
-        console.log("Item", pool.coinTypeA, itemFeeA.toNumber(), feesA);
-        console.log("Item", pool.coinTypeB, itemFeeB.toNumber(), feesB);*/
+        try {
 
-        let totalYield = rewards1 + rewards2 + feesA + feesB;
-        console.log(totalYield.toFixed(4));
+            for (let i = 0; i < 1000000; i++) {
+                const posRewardersAmount = await sdk.Rewarder.batchFetchPositionRewarders([posObjectId])
+                const fees = await sdk.Position.batchFetchPositionFees([posObjectId])
 
-        // write value to file
-        let fs = require('fs');
-        fs.writeFileSync('totalYield.txt', totalYield.toFixed(4));
+                //console.log("posRewardersAmount", JSON.stringify(posRewardersAmount, null, 2))
+                //console.log("fees", JSON.stringify(fees, null, 2))
 
-        await new Promise(r => setTimeout(r, 3000));
+                let item1 = posRewardersAmount[posObjectId][0];
+                let item2 = posRewardersAmount[posObjectId][1];
+                let itemFeeA = fees[posObjectId].feeOwedA;
+                let itemFeeB = fees[posObjectId].feeOwedB;
+
+                let rewards1 = toDollars(item1.coin_address, item1.amount_owed.toNumber());
+                let rewards2 = toDollars(item2.coin_address, item2.amount_owed.toNumber());
+                let feesA = toDollars(pool.coinTypeA, itemFeeA.toNumber());
+                let feesB = toDollars(pool.coinTypeB, itemFeeB.toNumber());
+
+                /*console.log("Item", item1.coin_address, item1.amount_owed.toNumber(), rewards1);
+                console.log("Item", item2.coin_address, item2.amount_owed.toNumber(), rewards2);
+                console.log("Item", pool.coinTypeA, itemFeeA.toNumber(), feesA);
+                console.log("Item", pool.coinTypeB, itemFeeB.toNumber(), feesB);*/
+
+                let totalYield = rewards1 + rewards2 + feesA + feesB;
+                console.log(totalYield.toFixed(4));
+
+                // write value to file
+                let fs = require('fs');
+                fs.writeFileSync('totalYield.txt', totalYield.toFixed(4));
+
+                await new Promise(r => setTimeout(r, 3000));
+            }
+        }
+        catch (e) {
+            console.log("ERROR", e);
+            await new Promise(r => setTimeout(r, 3000));
+        }
     }
 
     //console.log("STOPPED1");
